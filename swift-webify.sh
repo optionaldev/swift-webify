a72f134fa1628eb3b85e3892dc4823e031eeeb4b
佐藤悠真 <archyusa@icloud.com>
2021-05-23 20:44:47 +0200

    Replaced span with 1 letter alternatives to save reduce html file sizes

--- swift-webify.sh
+++ swift-webify.sh
@@ -7,29 +7,28 @@ input=$(<input.txt)
 input="$(sed "s/</\&lt;/" <<< "$input")"
 input="$(sed "s/>/\&gt;/" <<< "$input")"
 
+
+#   input="$(sed -E "s/([[:alnum:][:space:]])\=([[:alnum:][:space:]])/\1<span class=\"operator\">\=<\/span>\2/g" <<< "$input")"
+
 ####################
 # Keyword handling #
 ####################
 
 echo "Colorizing keywords.."
 
-keywords=("class" "false" "func" "if" "in" "init" "import" "let" "nil" "private" "return" "self" "some" "struct" "true" "var")
+keywords=("class" "didSet" "else" "false" "final" "func" "if" "in" "init" "import" "let" "nil" "private" "return" "self" "some" "static" "struct" "true" "var")
 
 for keyword in ${keywords[@]} 
 do
   # Capture and replace when not at the beginning nor the end 
-  input="$(sed "s/\([^[:alnum:]]\)\($keyword\)\([^[:alnum:]]\)/\1<span _cl_ass_=\"keyword\">\2<\/span>\3/" <<< "$input")"
+  input="$(sed "s/\([(|[:space:]]\)\($keyword\)\([[:space:]|)|(|,]\)/\1<a>\2<\/a>\3/" <<< "$input")"
   
   # Capture and replace at the beginning of the line
-  input="$(sed -E "s/^($keyword)/<span _cl_ass_=\"keyword\">\1<\/span>/" <<< "$input")"
+  input="$(sed -E "s/^($keyword)/<a>\1<\/a>/" <<< "$input")"
   
   # Capture and replace at the end of the line
-  input="$(sed -E "s/($keyword)$/<span _cl_ass_=\"keyword\">\1<\/span>/" <<< "$input")"
+  input="$(sed -E "s/($keyword)$/<a>\1<\/a>/" <<< "$input")"
 done
-
-# Final capture and replace because we used a placeholder class name due to the
-# word "class" being a keyword in both the keyword list and the html syntax
-input="$(sed "s/_cl_ass_/class/g" <<< "$input")"
 
 ###########################
 # Built-in types handling #
@@ -37,15 +36,15 @@ input="$(sed "s/_cl_ass_/class/g" <<< "$input")"
 
 echo "Colorizing built-in types.."
 
-builtinTypes=("Binding" "Bool" "Color" "Context"  "NotificationCenter" "NSObject" "String" "State" "UITextField" "UITextFieldDelegate" "UIViewRepresentable" "UIViewRepresentableContext" "View" "VStack")
+builtinTypes=("Binding" "Bool" "Color" "Coordinator" "Context" "NotificationCenter" "NSObject" "ObservableObject" "ObservedObject" "NSRange" "NSString" "Published" "String" "State" "UITextField" "UITextFieldDelegate" "UIViewRepresentable" "UIViewRepresentableContext" "View" "VStack")
 
 for builtinType in ${builtinTypes[@]} 
 do
-  input="$(sed "s/\([^[:alnum:]]\)\($builtinType\)\([^[:alnum:]]\)/\1<span class=\"builtinType\">\2<\/span>\3/" <<< "$input")"
+  input="$(sed "s/\([^[:alnum:]]\)\($builtinType\)\([^[:alnum:]]\)/\1<b>\2<\/b>\3/" <<< "$input")"
   
-  input="$(sed -E "s/^($builtinType)/<span class=\"builtinType\">\1<\/span>/" <<< "$input")"
+  input="$(sed -E "s/^($builtinType)/<b>\1<\/b>/" <<< "$input")"
   
-  input="$(sed -E "s/($builtinType)$/<span class=\"builtinType\">\1<\/span>/" <<< "$input")"
+  input="$(sed -E "s/($builtinType)$/<b>\1<\/b>/" <<< "$input")"
 done
 
 ######################
@@ -54,28 +53,33 @@ done
 
 echo "Colorizing operators.."
 
-operators=("&&" "||" "$")
+operators=("&&" "\|\|" "\\$" "==" "\?\?")
 
 for operator in ${operators[@]} 
 do
-  input="$(sed "s/\([[:alnum:]|[:space:]]\)\($operator\)\([[:alnum:]|[:space:]]\)/\1<span class=\"operator\">\2<\/span>\3/g" <<< "$input")"
+  input="$(sed -E "s/($operator)/<u>\1<\/u>/g" <<< "$input")"
 done
 
-input="$(sed "s/\([[:alnum:]|[:space:]]\)!\([[:alnum:]|[:space:]]\)/\1<span class=\"exclamation\">!<\/span>\2/g" <<< "$input")"
+# We want to color _ but only those that represent bindings
+input="$(sed "s/\([^[:alnum:]]\)_\([[:alnum:]]\)/\1<u>_<\/u>\2/g" <<< "$input")"
+
+# We don't want to color the != as well
+input="$(sed "s/\([[:alnum:]|[:space:]]\)!\([[:alnum:]|[:space:]]\)/\1<p>!<\/p>\2/g" <<< "$input")"
+
 
 ############################
 # Built-in values handling #
 ############################
 
-echo "Colorizing other methods.."
+echo "Colorizing other built-in methods.."
 
-builtinValues=("addObserver" "autocapitalizationType" "background" "becomeFirstResponder" "center" "default" "coordinator" "delegate" "main" "none" "onChange" "red" "text" "textAlignment" "textDidChangeNotification" "zero")
+builtinValues=("addObserver" "autocapitalizationType" "background" "blue" "becomeFirstResponder" "center" "default" "coordinator" "delegate" "font" "isFirstResponder" "main" "none" "onChange" "red" "removeObserver" "replacingCharacters" "resignFirstResponder" "systemFont" "text" "textAlignment" "textColor" "textDidChangeNotification" "zero")
 
 for builtinValue in ${builtinValues[@]} 
 do
-  input="$(sed "s/\.\($builtinValue\)\([^[:alnum:]]\)/\.<span class=\"otherBuiltin\">\1<\/span>\2/g" <<< "$input")"
+  input="$(sed "s/\.\($builtinValue\)\([^[:alnum:]]\)/\.<i>\1<\/i>\2/g" <<< "$input")"
   
-  input="$(sed -E "s/\.($builtinValue)$/\.<span class=\"otherBuiltin\">\1<\/span>/" <<< "$input")"
+  input="$(sed -E "s/\.($builtinValue)$/\.<i>\1<\/i>/" <<< "$input")"
 done
 
 ######################################
@@ -88,34 +92,34 @@ builtinMethods=("print")
 
 for builtinMethod in ${builtinMethods[@]} 
 do
-  input="$(sed "s/\.\($builtinMethod\)\([^[:alnum:]]\)/\.<span class=\"otherBuiltin\">\1<\/span>\2/g" <<< "$input")"
-  
-  input="$(sed -E "s/\.($builtinMethod)$/\.<span class=\"otherBuiltin\">\1<\/span>/" <<< "$input")"
+  input="$(sed "s/\([[:space:]]\)\($builtinMethod\)(/\1<i>\2<\/i>(/g" <<< "$input")"
 done
 
 #################
 # func handling #
 #################
 
-methods=("backwardUpdate" "forwardUpdate" "makeCoordinator" "makeUIView" "updateUIView")
+methods=("backwardUpdate" "dismantleUIView" "forwardUpdate" "makeCoordinator" "makeUIView" "textField" "updateUIView")
 
 for method in ${methods[@]} 
 do
-  input="$(sed "s/\($method\)/<span class=\"method\">\1<\/span>/g" <<< "$input")"
+  input="$(sed "s/\([[:space:]]\)\($method\)(/\1<s>\2<\/s>(/g" <<< "$input")"
 done
 
 ######################
 # constants handling #
 ######################
 
-constants=("\"\"")
+# constants=("\"\"")
 
-for constant in ${constants[@]} 
-do
-  input="$(sed "s/\([[:alnum:]|[:space:]]\)\($constant\)\([[:alnum:]|[:space:]]\)/\1<span class=\"constant\">\2<\/span>\3/g" <<< "$input")"
-  
-  input="$(sed -E "s/($constant)$/<span class=\"constant\">\1<\/span>/" <<< "$input")"
-done
+# Handle when "" is somewhere in the middle of the line of code, like before a && or ||
+input="$(sed "s/\([^[:alnum:]]\)\"\"\([^[:alnum:]]\)/\1<q>\"\"<\/q>\2/g" <<< "$input")"
+
+# Handle when there is text between ""
+input="$(sed "s/\"\([^\"].*\)\"/<q>\"\1\"<\/q>/" <<< "$input")"
+
+# Handle when "" is the final thing on the line of code
+input="$(sed -E "s/\"\"$/<q>\"\"<\/q>/" <<< "$input")"
 
 # write to output file
 echo "$input" >output.txt
